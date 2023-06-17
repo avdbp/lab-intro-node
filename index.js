a621c6c1587ee35a2e924d1994393251c72d5bf0
avdbp <alejandro.vdb@gmail.com>
2023-06-17 14:48:27 +0200

    lab terminado ;-)

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((itemA, itemB) => {
+      if (itemA > itemB) {
+        return 1;
+      } else if (itemA < itemB) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
 
-  get(pos) {}
+    this.length = this.items.length;
+  }
 
-  max() {}
 
-  min() {}
+  get(pos) {
+    if(pos > this.items.length -1){
+      throw new Error('OutOfBounds');
+    }
+    else{
+      return this.items[pos];
+    }
+  }
 
-  sum() {}
+  max() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }else {
+      return this.items[this.length -1];
+    }
+  }
 
-  avg() {}
-}
+  min() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }else {
+      return this.items[0];
+    }
+  }
+  sum() {
+    return this.items.reduce((acumulator, currentValue) => {
+      return acumulator + currentValue;
+    }, 0);
+  }
+
+  avg() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }else {
+      return this.sum() / this.length;
+      };
+    }
+    
+  }
 
 module.exports = SortedList;
